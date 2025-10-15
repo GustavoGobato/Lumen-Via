7f3a79f5c3b4b609add50a47f39a300c3901f431
Arthur Roberto Alves <arthurrobertoalves@gmail.com>
2025-10-14 21:15:59 -0300

    uptade

---
+++ mobilidade/scripts/feceap.js
@@ -0,0 +1,6 @@
+const maps = () => {
+  window.open(
+    "https://www.google.com/maps/dir//CEAP+-+Centro+Educacional+Assistencial+Profissionalizante+-+R.+José+Viêira+Martins,+270+-+Jardim+Itapura,+São+Paulo+-+SP,+04466-025/",
+    "_blank"
+  );
+};
